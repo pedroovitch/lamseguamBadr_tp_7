1962a56b8aa00c9fadeb6106f9019433c22b8e37
pedrovitch <pedrovitc00@gmail.com>
2026-03-10 16:23:42 +0100

    hh

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        
         $this->call([
         FilmsTableSeeder::class,
         ActeursTableSeeder::class,
